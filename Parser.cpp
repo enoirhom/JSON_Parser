f5fe0ea49615f6d24c787603265c98feadb8f247
enoirhom <emil.noirhomme@student.unamur.be>
2020-11-25 17:39:49 +0100

    create Parser class

---
+++ Parser.cpp
@@ -0,0 +1,80 @@
+#include "Parser.h"
+
+Parser::Parser(Lexer &lexer) : m_Input(lexer), m_Lookahead(nullptr) {
+    consume();
+    l.printOLC("Create Parser");
+}
+
+Parser::~Parser() {
+    l.printOLC("Delete Parser");
+}
+
+void Parser::json() {
+    value();
+}
+
+void Parser::obj() {
+    match(Lexer::LCURL);
+    if (m_Lookahead->m_Type == Lexer::STRING) {
+        pair();
+        while (m_Lookahead->m_Type == Lexer::COMMA) {
+            match(Lexer::COMMA);
+            pair();
+        }
+    }
+    match(Lexer::RCURL);
+}
+
+void Parser::pair() {
+    match(Lexer::STRING);
+    match(Lexer::COLON);
+    value();
+}
+
+void Parser::arr() {
+    match(Lexer::LBRACK);
+    if (lookForValue()) {
+        value();
+        while (m_Lookahead->m_Type == Lexer::COMMA) {
+            match(Lexer::COMMA);
+            value();
+        }
+    }
+    match(Lexer::RBRACK);
+}
+
+void Parser::value() {
+    if (m_Lookahead->m_Type == Lexer::STRING) {
+        match(Lexer::STRING);
+    } else if (m_Lookahead->m_Type == Lexer::NUMBER) {
+        match(Lexer::NUMBER);
+    } else if (m_Lookahead->m_Type == Lexer::LCURL) {
+        obj();
+    } else if (m_Lookahead->m_Type == Lexer::LBRACK) {
+        arr();
+    } else {
+        throw "error Parser::value()";
+    }
+}
+
+void Parser::match(int type) {
+    if (m_Lookahead->m_Type == type) {
+        consume();
+    } else {
+        throw "error Parser::match()";
+    }
+}
+
+void Parser::consume() {
+    if (m_Lookahead != nullptr) {
+        delete m_Lookahead;
+    }
+    m_Lookahead = m_Input.nextToken();
+}
+
+bool Parser::lookForValue() {
+    return m_Lookahead->m_Type == Lexer::STRING ||
+            m_Lookahead->m_Type == Lexer::NUMBER ||
+            m_Lookahead->m_Type == Lexer::LCURL ||
+            m_Lookahead->m_Type == Lexer::LBRACK;
+}
---
+++ Parser.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Lexer.h"
+#include "Token.h"
+#include "Log.h"
+
+class Parser {
+public:
+    Parser(Lexer &lexer);
+    ~Parser();
+    void json();
+    void obj();
+    void pair();
+    void arr();
+    void value();
+
+private:
+    Lexer &m_Input;
+    Token *m_Lookahead;
+
+    void match(int type);
+    void consume();
+
+    bool lookForValue();
+};
